323c4f36016b77f5651148b59498572166da844d
Lee Gayeon <52454824+gayomii@users.noreply.github.com>
2023-02-03 10:16:48 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,5 +1,7 @@
 function solution(s) {
+    if(typeof s !== 'string') throw new Error('error.s is not a string type')
+    
     let strList = s.split(' ')
-    strList = strList.map(str => str ? str[0].toUpperCase() + str.slice(1, str.length).toLowerCase() : str)
+    strList = strList.map(str => str ? str[0].toUpperCase() + str.slice(1).toLowerCase() : str)
     return strList.join(' ')
 }
