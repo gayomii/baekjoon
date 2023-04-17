800b69ec16840450a6fe0c78065927c407151673
Lee Gayeon <52454824+gayomii@users.noreply.github.com>
2023-04-17 17:03:58 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 4.41 ms, Memory: 34.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -0,0 +1,38 @@
+function solution(today, terms, privacies) {
+    let answer = []
+    const termsObj = {}
+    
+    terms.forEach(term => {
+        const [type, month] = term.split(' ')
+        termsObj[type] = parseInt(month)
+    })
+    
+    privacies.forEach((privacy, idx) => {
+        const [date, type] = privacy.split(' ')
+        
+        if(calExpirationDate(date, termsObj[type]) <= new Date(today)) answer.push(idx+1)
+    })
+    return answer
+}
+
+function calExpirationDate(strDate, term) {
+    const date = new Date(strDate)
+    
+    let year = date.getFullYear()
+    let month = date.getMonth() + 1
+    let day = date.getDate()
+    
+    if(month + term > 12) {
+        year += parseInt((month + term)/12)
+        month = (month + term)%12
+        
+        if(!month) {
+            year --
+            month = 12
+        }
+    } else month += term
+
+    
+    console.log(`${year}.${month}.${day}`)
+    return new Date(`${year}.${month}.${day}`)
+}
