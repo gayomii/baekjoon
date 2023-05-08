32ec10f7b0a034f7cf96ad0c602b43edce6f1a22
Lee Gayeon <52454824+gayomii@users.noreply.github.com>
2023-05-08 21:46:35 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,18 +1,16 @@
 function solution(survey, choices) {
     let answer = ''
-    let mbti = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 }
+    let mbti = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0}
     
-    choices.forEach((choice, idx) => {
-        const score = choice - 4
-        const [type1, type2] = survey[idx]
-        
-        score < 0 ? mbti[type1] += score*-1 : mbti[type2] += score
-    })
-    
-    mbti['R'] >= mbti['T'] ? answer += 'R' : answer += 'T'
-    mbti['C'] >= mbti['F'] ? answer += 'C' : answer += 'F'
-    mbti['J'] >= mbti['M'] ? answer += 'J' : answer += 'M'
-    mbti['A'] >= mbti['N'] ? answer += 'A' : answer += 'N'
+    for(let i=0; i<survey.length; i++) {
+        const result = choices[i]-4
+        result < 0 ? mbti[survey[i][0]] += Math.abs(result) :  mbti[survey[i][1]] += result
+    }
 
+    mbti['R'] >= mbti['T'] ? answer+= 'R' : answer+='T'
+    mbti['C'] >= mbti['F'] ? answer+= 'C' : answer+='F'
+    mbti['J'] >= mbti['M'] ? answer+= 'J' : answer+='M'
+    mbti['A'] >= mbti['N'] ? answer+= 'A' : answer+='N'
+    
     return answer
 }
