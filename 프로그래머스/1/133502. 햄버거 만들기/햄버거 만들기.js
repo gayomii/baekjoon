2af6298a5e37119dc9cd1a6bcd79e023587c48fa
Lee Gayeon <52454824+gayomii@users.noreply.github.com>
2024-02-29 13:39:54 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,20 @@
+function solution(ingredient) {
+    let answer = 0
+    let stack = []
+    
+    // 빵-야채-고기-빵 (1,2,3,1)
+    for(let i=0; i<ingredient.length; i++) {
+        stack.push(ingredient[i])
+        if(stack.length >= 4) {
+            if(ingredient[i] === 1) {
+                if(stack.slice(-4).join('') === '1231') {
+                stack.splice(-4)
+                answer++
+            }
+            }
+            
+        }
+    }
+    
+    return answer
+}
