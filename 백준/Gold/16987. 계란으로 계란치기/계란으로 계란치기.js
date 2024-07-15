fd49450340d0717a5d4a8d0a849b926e0438b73f
Lee Gayeon <52454824+gayomii@users.noreply.github.com>
2024-07-15 23:50:06 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 1540 ms, Memory: 12720 KB -BaekjoonHub

---
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const totalNum = Number(input.splice(0, 1));
+let answer = 0;
+
+const eggs = input.map((data) => data.split(' ').map((value) => Number(value)));
+
+function solution(targetIdx) {
+  answer = Math.max(answer, eggs.filter((egg) => egg[0] <= 0).length);
+
+  if (targetIdx === totalNum) return;
+
+  if (eggs[targetIdx][0] <= 0) {
+    solution(targetIdx + 1);
+    return;
+  }
+
+  for (let i = 0; i < totalNum; i++) {
+    if (eggs[i][0] <= 0 || targetIdx === i) continue;
+
+    eggs[targetIdx][0] -= eggs[i][1];
+    eggs[i][0] -= eggs[targetIdx][1];
+
+    solution(targetIdx + 1);
+
+    eggs[targetIdx][0] += eggs[i][1];
+    eggs[i][0] += eggs[targetIdx][1];
+  }
+}
+
+solution(0);
+console.log(answer);
