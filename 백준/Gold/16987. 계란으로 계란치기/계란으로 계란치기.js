2977dfab9c0cf73e21229cc73c8b08b5362a6780
Lee Gayeon <52454824+gayomii@users.noreply.github.com>
2024-07-16 00:35:20 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 1408 ms, Memory: 13192 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.js
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.js
@@ -1,33 +1,65 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const readline = require('readline');
 
-const totalNum = Number(input.splice(0, 1));
-let answer = 0;
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-const eggs = input.map((data) => data.split(' ').map((value) => Number(value)));
+const data = [];
 
-function solution(targetIdx) {
-  answer = Math.max(answer, eggs.filter((egg) => egg[0] <= 0).length);
+rl.on('line', (input) => {
+  data.push(input);
+}).on('close', () => {
+  const checkBreakCount = (info) => {
+    return info.filter((i) => i[0] <= 0).length;
+  };
 
-  if (targetIdx === totalNum) return;
+  const N = +data.shift();
+  const info = data.map((i) => i.split(' ').map(Number));
 
-  if (eggs[targetIdx][0] <= 0) {
-    solution(targetIdx + 1);
-    return;
-  }
+  let maxValue = 0;
+  const dfs = (index, info) => {
+    if (index === N) {
+      const currentBreakCount = checkBreakCount(info);
+      if (maxValue < currentBreakCount) maxValue = currentBreakCount;
+      return;
+    }
 
-  for (let i = 0; i < totalNum; i++) {
-    if (eggs[i][0] <= 0 || targetIdx === i) continue;
+    const currentEgg = info[index];
 
-    eggs[targetIdx][0] -= eggs[i][1];
-    eggs[i][0] -= eggs[targetIdx][1];
+    // 현재 계란으로 다른 계란을 칠 수 있는지 여부
+    let hitFlag = false;
 
-    solution(targetIdx + 1);
+    for (let i = 0; i < info.length; i++) {
+      // 현재 선택한 계란이 깨져있거나, 대상이 되는 계란이 깨졌다면 패스
+      const targetEgg = info[i];
+      if (i === index || currentEgg[0] <= 0 || targetEgg[0] <= 0) continue;
 
-    eggs[targetIdx][0] += eggs[i][1];
-    eggs[i][0] += eggs[targetIdx][1];
-  }
-}
+      hitFlag = true;
 
-solution(0);
-console.log(answer);
+      currentEgg[0] -= targetEgg[1];
+      targetEgg[0] -= currentEgg[1];
+      info[index][0] = currentEgg[0];
+      info[i][0] = targetEgg[0];
+
+      dfs(index + 1, info);
+
+      currentEgg[0] += targetEgg[1];
+      targetEgg[0] += currentEgg[1];
+      info[index][0] = currentEgg[0];
+      info[i][0] = targetEgg[0];
+    }
+
+    // 계란을 던질수 없을때 (현재 선택한 계란이 깨져있거나, 다른 계란도 모두 깨져있을때)
+    // 백트래킹 탐색을 종료하는게 아니라 다음 탐색으로 넘어간다.
+    if (!hitFlag) {
+      dfs(index + 1, info);
+    }
+  };
+
+  dfs(0, info);
+
+  console.log(maxValue);
+
+  process.exit();
+});
